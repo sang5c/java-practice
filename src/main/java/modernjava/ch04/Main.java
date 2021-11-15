9c67b9b1a78de4439a80c0b94cefe7cd1269dc06
sang5c <skah321@gmail.com>
2021-11-15 19:09:00 +0900

    4.3.1 딱 한번만 탐색할 수 있다.

---
+++ src/main/java/modernjava/ch04/Main.java
@@ -0,0 +1,33 @@
+package modernjava.ch04;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        example4_2();
+    }
+
+    private static void example4_2() {
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.MEAT),
+                new Dish("salmon", false, 450, Dish.Type.MEAT)
+        );
+
+        List<String> threeHighCaloricDishNames = menu.stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println(threeHighCaloricDishNames);
+    }
+
+}
