3e64b43953c4a513394d6d3873051674da699374
sang5c <skah321@gmail.com>
2021-11-16 19:08:27 +0900

    4.4.2 최종 연산

--- src/main/java/modernjava/ch04/Main.java
+++ src/main/java/modernjava/ch04/Main.java
@@ -1,15 +1,13 @@
 package modernjava.ch04;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        example4_2();
-    }
-
-    private static void example4_2() {
         List<Dish> menu = Arrays.asList(
                 new Dish("pork", false, 800, Dish.Type.MEAT),
                 new Dish("beef", false, 700, Dish.Type.MEAT),
@@ -22,6 +20,50 @@ public class Main {
                 new Dish("salmon", false, 450, Dish.Type.MEAT)
         );
 
+        example4_2(menu);
+        example4_3_2(menu);
+        quiz4_1(menu);
+    }
+
+    private static void quiz4_1(List<Dish> menu) {
+        // quiz: 스트림으로 변경
+        List<String> highCaloricDishes = new ArrayList<>();
+        Iterator<Dish> iterator = menu.iterator();
+        while (iterator.hasNext()) {
+            Dish dish = iterator.next();
+            if (dish.getCalories() > 300) {
+                highCaloricDishes.add(dish.getName());
+            }
+        }
+
+        // answer
+        List<String> collect = menu.stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+    }
+
+    private static void example4_3_2(List<Dish> menu) {
+        // 외부 반복
+        List<String> names = new ArrayList<>();
+        for (Dish dish : menu) {
+            names.add(dish.getName());
+        }
+
+        // 숨겨진 반복자를 이용한 외부 반복
+        Iterator<Dish> iterator = menu.iterator();
+        while (iterator.hasNext()) {
+            Dish dish = iterator.next();
+            names.add(dish.getName());
+        }
+
+        // 내부 반복
+        List<String> collect = menu.stream()
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+    }
+
+    private static void example4_2(List<Dish> menu) {
         List<String> threeHighCaloricDishNames = menu.stream()
                 .filter(dish -> dish.getCalories() > 300)
                 .map(Dish::getName)
