b7f1759c110a5ed8be384ddebf4448d43befc489
sang5c <skah321@gmail.com>
2021-11-17 19:46:05 +0900

    5.6 실전 연습

--- src/main/java/modernjava/ch05/Main.java
+++ src/main/java/modernjava/ch05/Main.java
@@ -3,6 +3,7 @@ package modernjava.ch05;
 import modernjava.ch04.Dish;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -28,6 +29,83 @@ public class Main {
         example5_5_1(numbers);
         example5_5_2(numbers);
         quiz5_3(menu);
+
+        example5_6();
+
+    }
+
+    private static void example5_6() {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        // 1. 2011년에 일어난 모든 트랜잭션을 찾아 오름차순
+        List<Transaction> quiz1 = transactions.stream()
+                .filter(t -> t.getYear() == 2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(Collectors.toList());
+        System.out.println(quiz1);
+
+        // 2. 거래자가 근무하는 모든 도시를 중복 없이 나열
+        List<String> quiz2 = transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getCity)
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(quiz2);
+
+        // 3. 케임브리지에 근무하는 모든 거래자를 찾아서 이름순으로 정렬
+        List<Trader> quiz3 = transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(t -> "Cambridge".equals(t.getCity()))
+                .distinct()
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList());
+        System.out.println(quiz3);
+
+        // 4. 모든 거래자의 이름을 알파벳순으로 정렬, concat
+        String quiz4 = transactions.stream()
+                .map(t -> t.getTrader().getName())
+                .distinct()
+                .sorted()
+                .reduce("", (s, s2) -> s + s2);
+        // joining()
+        System.out.println(quiz4);
+
+        // 5. 밀라노에 거래자가 있나?
+        boolean milan = transactions.stream()
+                .anyMatch(t -> t.getTrader().getCity().equals("Milan"));
+        System.out.println(milan);
+
+        // 6. 케임브리지에 거주하는 거래자의 모든 트랜잭션값 출력
+        transactions.stream()
+                .filter(t -> t.getTrader().getCity().equals("Cambridge"))
+                .map(Transaction::getValue)
+                .forEach(System.out::println);
+
+        // 7. 전체 트랙잭션중 최대값
+        Integer max = transactions.stream()
+                .map(Transaction::getValue)
+                .max(Integer::compareTo)
+                .get();
+        System.out.println(max);
+
+        // 8. 전체 트랙잭션중 최소값
+        Optional<Integer> quiz8 = transactions.stream()
+                .map(Transaction::getValue)
+                .reduce((v1, v2) ->  v1 < v2 ? v1 : v2);
+        // transactions.stream().min(Comparator.comparing(Transaction::getValue));
+        System.out.println(quiz8);
     }
 
     // map reduce 패턴
