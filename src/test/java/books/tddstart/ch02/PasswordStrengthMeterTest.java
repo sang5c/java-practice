594b20b22650508051aea037f3ee10a749ebd874
sang5c <skah321@gmail.com>
2021-12-06 19:39:08 +0900

    ch03

--- src/test/java/books/tddstart/ch02/PasswordStrengthMeterTest.java
+++ src/test/java/books/tddstart/ch02/PasswordStrengthMeterTest.java
@@ -28,4 +28,12 @@ class PasswordStrengthMeterTest {
         assertEquals(PasswordStrength.NORMAL, result2);
     }
 
+    @DisplayName("숫자를 포함하지 않고 나머지 조건은 충족")
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab!@ABqwer");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
+
 }
