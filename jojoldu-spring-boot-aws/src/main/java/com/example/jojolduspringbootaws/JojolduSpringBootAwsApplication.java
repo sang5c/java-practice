ab36f8cc335e91fa99e4f44f323d4b29070090d3
sang5c <skah321@gmail.com>
2021-12-25 18:33:44 +0900

    init jojoldu-spring-boot-aws

---
+++ jojoldu-spring-boot-aws/src/main/java/com/example/jojolduspringbootaws/JojolduSpringBootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jojolduspringbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojolduSpringBootAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JojolduSpringBootAwsApplication.class, args);
+    }
+
+}
---
+++ jojoldu-spring-boot-aws/src/test/java/com/example/jojolduspringbootaws/JojolduSpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jojolduspringbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojolduSpringBootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
